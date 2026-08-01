841df37d055034741618c0a2963bd2c22476e7df
Bede16 <16863675+Bede16@users.noreply.github.com>
2026-08-01 03:14:31 +0200

    Use JSON5 configuration loader

--- src/main/java/io/github/bede16/wildpaths/WildPaths.java
+++ src/main/java/io/github/bede16/wildpaths/WildPaths.java
@@ -1,10 +1,8 @@
 package io.github.bede16.wildpaths;
 
-import io.github.bede16.wildpaths.config.WildPathsConfig;
 import io.github.bede16.wildpaths.event.WildPathsEvents;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
@@ -16,7 +14,6 @@ public final class WildPaths {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public WildPaths(ModContainer modContainer) {
-        modContainer.registerConfig(ModConfig.Type.COMMON, WildPathsConfig.SPEC);
         NeoForge.EVENT_BUS.register(new WildPathsEvents());
     }
 }
